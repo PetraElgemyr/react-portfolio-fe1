b2267a393a948e0864db9a76d59f5edcaf577ec6
PetraElgemyr <petra.elgemyr@gmail.com>
2024-11-28 09:43:20 +0100

    add pages routes

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { HomePage } from "./components/pages/HomePage";
+import { ContactPage } from "./components/pages/ContactPage";
+import { AboutPage } from "./components/pages/AboutPage";
+import { ProjectsPage } from "./components/pages/ProjectsPage";
 
 export const Routing = () => {
   return (
     <Router basename="/">
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
       </Routes>
     </Router>
   );
