c499b24b51934e04a9dd1b0d7238abe5a33c25b6
PetraElgemyr <petra.elgemyr@gmail.com>
2024-11-28 10:43:21 +0100

    install react three fiber, cannon, drei and add nametext

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,7 +1,24 @@
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import { NameText } from "../NameText";
+
 export const HomePage = () => {
   return (
-    <>
-      <h1>My home page</h1>
-    </>
+    <div>
+      <Canvas camera={{ position: [0, 0, 8], fov: 100 }}>
+        <OrbitControls
+          enableZoom={false}
+          autoRotate={false}
+          autoRotateSpeed={0.5}
+          enablePan={false}
+          zoomSpeed={0.25}
+          dampingFactor={0.1}
+        />
+        <Suspense fallback={"Loading"}>
+          <NameText />
+        </Suspense>
+      </Canvas>
+    </div>
   );
 };
