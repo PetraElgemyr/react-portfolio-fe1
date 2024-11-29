18a3016aa04bbc37aa548a4443ea785887269d84
PetraElgemyr <petra.elgemyr@gmail.com>
2024-11-29 14:17:48 +0100

    create and add skill container and make h1 centered

--- src/components/pages/SkillsPage.tsx
+++ src/components/pages/SkillsPage.tsx
@@ -1,7 +1,15 @@
+import { ColCentered, ColFlexedCenter } from "../../styled/Common/Common";
+import { SkillContainer } from "../../styled/Skills/SkillContainer";
+
 export const SkillsPage = () => {
   return (
     <>
-      <h1>Skills page</h1>
+      <ColCentered>
+        <h1>Skills page</h1>
+      </ColCentered>
+      <ColFlexedCenter>
+        <SkillContainer></SkillContainer>
+      </ColFlexedCenter>
     </>
   );
 };
---
+++ src/styled/Skills/SkillContainer.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const SkillContainer = styled.article`
+  width: 300px;
+  height: 200px;
+  border: 1px solid white;
+`;
