756813e7227af37f18f4179a884b6423fc0ff8cb
PetraElgemyr <petra.elgemyr@gmail.com>
2024-11-30 23:47:19 +0100

    add padding to project container

--- src/styled/Portfolio/ProjectContainer.tsx
+++ src/styled/Portfolio/ProjectContainer.tsx
@@ -5,19 +5,23 @@ export const ProjectContainer = styled.section`
   width: 100%;
   border-radius: 5px 5px 0 0;
   box-shadow: 0 6px 12px #ffffff42;
+  padding-bottom: 8%;
 
   @media screen and (${devices.tablet}) {
     width: 75%;
     border-radius: 10px 10px 0 0;
+    padding-bottom: 4%;
   }
 
   @media screen and (${devices.laptop}) {
     width: 45%;
     box-shadow: 0 6px 15px #ffffff42;
+    padding-bottom: 3%;
   }
 
   @media screen and (${devices.laptopL}) {
     border-radius: 15px 15px 0 0;
+    padding-bottom: 2%;
   }
 
   @media screen and (${devices.fourK}) {
