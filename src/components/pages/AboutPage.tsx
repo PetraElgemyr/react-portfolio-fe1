cccd375c3acd428ac9651694c106ebce0e0c941c
PetraElgemyr <petra.elgemyr@gmail.com>
2024-12-01 01:14:49 +0100

    add topp margin to all pages

--- src/components/pages/AboutPage.tsx
+++ src/components/pages/AboutPage.tsx
@@ -1,8 +1,12 @@
+import { PageMarginTop } from "../../styled/Common/Common";
+
 export const AboutPage = () => {
   return (
     <>
-      <h1>About page. hejhej</h1>
-      <p>hej en p-tag</p>
+      <PageMarginTop>
+        <h1>About page. hejhej</h1>
+        <p>hej en p-tag</p>{" "}
+      </PageMarginTop>
     </>
   );
 };
