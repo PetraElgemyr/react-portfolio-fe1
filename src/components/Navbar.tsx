39ec064097da54f76982979a6a380d177666f288
PetraElgemyr <petra.elgemyr@gmail.com>
2024-12-01 01:38:11 +0100

    make menu close when clicked outside menu and hb icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,16 @@ import { Link } from "react-router-dom";
 import { Pages } from "./enums/Pages";
 import { LinkUrls } from "./enums/LinkUrls";
 import "../scss/navbar.scss";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+  const hamburgerRef = useRef<HTMLDivElement>(null);
 
   const toggleMenu = () => {
+    console.log(menuOpen, "sätter till ", !menuOpen);
+
     setMenuOpen(!menuOpen);
   };
 
@@ -15,12 +19,31 @@ export const Navbar = () => {
     setMenuOpen(false);
   };
 
+  useEffect(() => {
+    const handleClickOutsideMenu = (e: MouseEvent) => {
+      if (
+        menuRef.current &&
+        !menuRef.current.contains(e.target as Node) &&
+        hamburgerRef.current &&
+        !hamburgerRef.current.contains(e.target as Node)
+      ) {
+        closeMenu();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutsideMenu);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutsideMenu);
+    };
+  }, []);
+
   return (
     <>
-      <div className="hamburger" onClick={toggleMenu}>
+      <div ref={hamburgerRef} className="hamburger" onClick={toggleMenu}>
         <i className={`fa-solid ${menuOpen ? "fa-times" : "fa-bars"}`}></i>
       </div>
-      <nav className={`navbar${menuOpen ? "-open" : ""}`}>
+      <nav ref={menuRef} className={`navbar${menuOpen ? "-open" : ""}`}>
         <ul>
           <li className="nav-item">
             <Link to={LinkUrls.HOME} onClick={closeMenu} className="nav-link">
