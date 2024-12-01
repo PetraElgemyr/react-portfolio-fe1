561ab5fddb7d390cac78489de960cc4323052f22
PetraElgemyr <petra.elgemyr@gmail.com>
2024-12-01 20:09:47 +0100

    add basic validation function and change contact form props

--- src/components/pages/ContactPage.tsx
+++ src/components/pages/ContactPage.tsx
@@ -5,11 +5,14 @@ import { StyledLink } from "../../styled/Common/StyledLink";
 import { ProjectCardText } from "../../styled/Portfolio/ProjectText";
 import { ContactForm } from "../ContactForm";
 import { FormEvent, useState } from "react";
+import { defaultFormData, IFormData } from "../interfaces/IFormData";
 
 export const ContactPage = () => {
   const [formSentSuccessfully, setFormSentSuccessfully] =
     useState<boolean>(false);
   const [formSubmitted, setFormSubmitted] = useState<boolean>(false);
+  const [formData, setFormData] = useState<IFormData>(defaultFormData);
+  const [errorMessage, setErrorMessage] = useState<string>("");
 
   const serviceId = import.meta.env.VITE_SERVICE_ID;
   const templateId = import.meta.env.VITE_TEMPLATE_ID;
@@ -23,13 +26,22 @@ export const ContactPage = () => {
       .then(() => {
         setFormSubmitted(true);
         setFormSentSuccessfully(true);
-        form.reset();
+        setFormData(defaultFormData);
+        setErrorMessage("");
       })
       .catch((error) => {
         setFormSubmitted(true);
         setFormSentSuccessfully(false);
         console.error("EmailJS error:", error);
       });
+  };
+
+  const validateForm = () => {
+    if (formData.fromEmail && formData.fromName && formData.message) {
+      return true;
+    }
+
+    return false;
   };
 
   return (
@@ -58,14 +70,26 @@ export const ContactPage = () => {
             </StyledLink>
           </ProjectCardText>
 
-          <ContactForm sendEmail={sendEmail} />
+          <ContactForm
+            sendEmail={sendEmail}
+            formData={formData}
+            setFormData={(data) => {
+              setFormData(data);
+            }}
+            validateForm={validateForm}
+            setErrorMessage={(message: string) => setErrorMessage(message)}
+          />
           {formSubmitted ? (
             <CenteredArticle>
               {formSentSuccessfully
                 ? "Message was successfully sent! I will get back as soon as possible."
                 : "Oops! Something went wrong, please try again"}
             </CenteredArticle>
-          ) : null}
+          ) : (
+            <CenteredArticle>
+              {errorMessage.length > 1 ? errorMessage : null}
+            </CenteredArticle>
+          )}
         </ColCentered>
       </PageMarginTop>
     </>
