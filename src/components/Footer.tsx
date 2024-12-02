0315e205576275e02dcc2a79d82bb14b9f9b5d5c
PetraElgemyr <petra.elgemyr@gmail.com>
2024-12-03 00:50:05 +0100

    add basic footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,18 @@
+import { FooterSection, ResponsiveFooter } from "../styled/ResponsiveFooter";
+
 export const Footer = () => {
-  return <></>;
+  return (
+    <>
+      <ResponsiveFooter>
+        <FooterSection>
+          <p>LinkedIn</p>
+          <p>Github</p>
+        </FooterSection>
+        <FooterSection>
+          <p>Email</p>
+          <p>Location: Stockholm, Sweden</p>
+        </FooterSection>
+      </ResponsiveFooter>
+    </>
+  );
 };
