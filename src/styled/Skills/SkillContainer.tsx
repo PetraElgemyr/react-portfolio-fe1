bca9813921822cf33bf105b95a38463f007e5034
PetraElgemyr <petra.elgemyr@gmail.com>
2024-12-03 01:27:07 +0100

    change skillcontainer styling

--- src/styled/Skills/SkillContainer.tsx
+++ src/styled/Skills/SkillContainer.tsx
@@ -1,7 +1,10 @@
 import styled from "styled-components";
 
 export const SkillContainer = styled.article`
-  width: 300px;
-  height: 200px;
-  border: 1px solid white;
+  width: 90%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
 `;
