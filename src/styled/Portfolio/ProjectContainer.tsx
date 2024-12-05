a4e60e186a0921c3e02c303969208718e3ff5103
PetraElgemyr <petra.elgemyr@gmail.com>
2024-12-05 14:13:01 +0100

    test to change styling on project container, border radius and shadow

--- src/styled/Portfolio/ProjectContainer.tsx
+++ src/styled/Portfolio/ProjectContainer.tsx
@@ -2,26 +2,27 @@ import styled from "styled-components";
 import { devices } from "../Variables/Devices";
 
 export const ProjectContainer = styled.section`
-  width: 100%;
-  border-radius: 5px 5px 2px 2px;
-  box-shadow: 0 6px 12px #ffffff42;
+  width: 90%;
+  border-radius: 2px;
+  // border: 1px solid #e9793a;
   padding-bottom: 8%;
+  box-shadow: 0 6px 12px #ffffff42;
 
   @media screen and (${devices.tablet}) {
-    width: 75%;
-    border-radius: 10px 10px 2px 2px;
+    width: 45%;
     padding-bottom: 4%;
   }
 
   @media screen and (${devices.laptop}) {
-    width: 45%;
-    box-shadow: 0 6px 15px #ffffff42;
+    width: 40%;
     padding-bottom: 3%;
+    // border: 2px solid #e9793a;
   }
 
   @media screen and (${devices.laptopL}) {
-    border-radius: 15px 15px 2px 2px;
+    border-radius: 4px;
     padding-bottom: 2%;
+    width: 30%;
   }
 
   @media screen and (${devices.fourK}) {
