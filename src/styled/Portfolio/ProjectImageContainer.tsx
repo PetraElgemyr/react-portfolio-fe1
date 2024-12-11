189fef8720489892a6e38cf85def0078fdde396a
PetraElgemyr <petra.elgemyr@gmail.com>
2024-12-11 01:18:52 +0100

    change border radius for repo images

--- src/styled/Portfolio/ProjectImageContainer.tsx
+++ src/styled/Portfolio/ProjectImageContainer.tsx
@@ -6,33 +6,21 @@ export const ProjectImageContainer = styled.div`
   margin: 0;
   padding: 0;
   object-fit: cover;
-  object-position: center;
-  border-radius: 5px 5px 0 0;
-
-  @media screen and (${devices.tablet}) {
-    border-radius: 10px 10px 0 0;
-  }
-
-  @media screen and (${devices.laptop}) {
-  }
-
-  @media screen and (${devices.laptopL}) {
-    border-radius: 15px 15px 0 0;
-  }
+  object-position: top center;
+  border-radius: 10px 10px 0 0;
 `;
 
 export const ProjectImage = styled.img`
   width: 100%;
   min-height: 200px;
   object-fit: cover;
-  object-position: center;
-  border-radius: 5px 5px 0 0;
+  object-position: top center;
+  border-radius: 10px 10px 0 0;
   margin: 0;
   padding: 0;
 
   @media screen and (${devices.tablet}) {
     max-height: 300px;
-    border-radius: 10px 10px 0 0;
   }
 
   @media screen and (${devices.laptop}) {
@@ -40,7 +28,6 @@ export const ProjectImage = styled.img`
   }
 
   @media screen and (${devices.laptopL}) {
-    border-radius: 15px 15px 0 0;
     height: 400px;
   }
 `;
