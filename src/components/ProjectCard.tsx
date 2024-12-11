4ab0a6c243a1a71e4bf324ec08b96e5089723774
PetraElgemyr <petra.elgemyr@gmail.com>
2024-12-11 01:19:13 +0100

    implement gradient border and black box background

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,4 +1,7 @@
-import { ProjectContainer } from "../styled/Portfolio/ProjectContainer";
+import {
+  BlackBox,
+  ProjectContainer,
+} from "../styled/Portfolio/ProjectContainer";
 import {
   ProjectImage,
   ProjectImageContainer,
@@ -22,35 +25,37 @@ export const ProjectCard = ({ project }: IProjectCardProps) => {
     <>
       {project ? (
         <ProjectContainer>
-          <ProjectImageContainer>
-            <ProjectImage src={project.img} alt={project.name}></ProjectImage>
-          </ProjectImageContainer>
-          <ProjectCardTitle>{project.name}</ProjectCardTitle>
-          <ProjectCardTextContainer>
-            <ProjectCardText>{project.description}</ProjectCardText>
-          </ProjectCardTextContainer>
-          <ProjectCardTextCenterContainer>
-            <StyledLink target="_blank" href={project.github}>
-              Github
-            </StyledLink>
-            {project.liveLink ? (
-              <StyledLink target="_blank" href={project.liveLink}>
-                Try it
+          <BlackBox>
+            <ProjectImageContainer>
+              <ProjectImage src={project.img} alt={project.name}></ProjectImage>
+            </ProjectImageContainer>
+            <ProjectCardTitle>{project.name}</ProjectCardTitle>
+            <ProjectCardTextContainer>
+              <ProjectCardText>{project.description}</ProjectCardText>
+            </ProjectCardTextContainer>
+            <ProjectCardTextCenterContainer>
+              <StyledLink target="_blank" href={project.github}>
+                Github
               </StyledLink>
-            ) : null}
+              {project.liveLink ? (
+                <StyledLink target="_blank" href={project.liveLink}>
+                  Try it
+                </StyledLink>
+              ) : null}
 
-            {/* todo sätt in skills bubblor */}
-          </ProjectCardTextCenterContainer>
+              {/* todo sätt in skills bubblor */}
+            </ProjectCardTextCenterContainer>
 
-          <SkillsContainer>
-            {project.techniques.length > 0 ? (
-              project.techniques.map((t, index) => (
-                <SkillBubble key={index}>{t}</SkillBubble>
-              ))
-            ) : (
-              <></>
-            )}
-          </SkillsContainer>
+            <SkillsContainer>
+              {project.techniques.length > 0 ? (
+                project.techniques.map((t, index) => (
+                  <SkillBubble key={index}>{t}</SkillBubble>
+                ))
+              ) : (
+                <></>
+              )}
+            </SkillsContainer>
+          </BlackBox>
         </ProjectContainer>
       ) : (
         <></>
